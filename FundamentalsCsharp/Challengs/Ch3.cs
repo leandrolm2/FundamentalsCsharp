22e97c9a53e5ac84ad1cca40c00d74167dfa381e
Leandrolm <leandromelgarejo@hotmail.com>
2024-03-02 09:47:51 -0300

    finished the challengs and organize the prompt

---
+++ FundamentalsCsharp/Challengs/Ch3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsCsharp.Challengs
+{
+    public class Ch3
+    {
+        public static void speedLimit() 
+        {
+            Console.WriteLine("Speed camera simulation.");
+            Console.Write("Type the speed limit.(ONLY THE NUMBERS, NOT THE KM): ");
+
+            int speedLimit = int.Parse(Console.ReadLine());
+
+            Console.Write("Type the speed of the car: ");
+
+            int carSpeed = int.Parse(Console.ReadLine());
+
+            if (carSpeed > speedLimit)
+            {
+                double demeritPoint = (carSpeed - speedLimit) / 5;
+                string hasLicense = demeritPoint > 12 ? $"License Suspended,  {demeritPoint} demerit points" : $"you have, {demeritPoint} demerit points";
+                Console.WriteLine(hasLicense);
+            }
+            else
+            {
+                Console.WriteLine($"{carSpeed} it's a acceptable velocity");
+            }
+        }
+    }
+}
--- FundamentalsCsharp/Program.cs
+++ FundamentalsCsharp/Program.cs
@@ -1,49 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-using FundamentalsCsharp.Class28;
+﻿using FundamentalsCsharp.Class28;
 using FundamentalsCsharp.Class28.Math;
 using FundamentalsCsharp.Class36_37;
 using static FundamentalsCsharp.Class36_37.Lesson2;
 
 namespace FundamentalsCsharp
 {
-
     class Program
     {
         public static void Main(string[] args)
         {
+            // Creating a Person object and introducing them
             Person leandro = new Person();
             leandro.FirstName = "Leandro";
             leandro.LastName = "Melgarejo";
             leandro.Age = 28;
-
             leandro.Introduce();
+            Console.WriteLine();
 
+            // Performing arithmetic operations using the Calculator class
             Calculator calculator = new Calculator();
-            //visual to whats is happening
+            // Visual representation of what's happening
             var result = calculator.Add(14, 14);
             Console.WriteLine(result);
 
-            //short way to do
+            // Short way to perform addition
             Console.WriteLine(calculator.Add(15, 15));
+            Console.WriteLine();
 
-            //Work with arrays
+            // Working with arrays
+            Console.WriteLine("Working with Arrays:");
             Class31.WorkingWithArray.Arrays();
+            Console.WriteLine();
 
-            //work with string
+            // Working with strings
+            Console.WriteLine("Working with Strings:");
             Class3233.Lessons.WorkWithString();
+            Console.WriteLine();
 
-            //work with enums
+            // Working with enums
+            Console.WriteLine("Working with Enums:");
             Class3435.Lessons.WorkWithEnums();
+            Console.WriteLine();
 
-            //PrimitiveTpes
+            // Working with non-primitive types
+            Console.WriteLine("Working with Non-Primitive Types:");
             Class36_37.Lessons.WorkWithNonAndPrimitiveTypes();
-            //The value stored in this number, located in a different memory location than the incremented number within the main method, lacks significance when accessed outside the main method.
+            // The value stored in this number, located in a different memory location than the incremented number within the main method,
+            // lacks significance when accessed outside the main method.
             var number = 1;
             Class36_37.Lesson2.Increment(number);
             Console.WriteLine(number);
+            Console.WriteLine();
 
-
-            // Create a new instance of the PersonAge class and initialize the Age property with the value 28
+            // Creating and using a PersonAge object
+            Console.WriteLine("Working with PersonAge:");
             PersonAge person = new PersonAge() { Age = 28 };
             Console.WriteLine(person.Age); // Print the current age of the person (should be 28)
 
@@ -52,15 +62,26 @@ namespace FundamentalsCsharp
 
             // Print the age of the person after calling the MakeOld method
             Console.WriteLine(person.Age); // The age should now be 38 (28 + 10)
+            Console.WriteLine();
 
-            //work if statements
+            // Working with if statements
+            Console.WriteLine("Working with If Statements:");
             Class41_42.Lessons.IfStates();
-            Class41_42.Lessons2.StateSwitch();
+            Console.WriteLine();
 
-            //challengs
+            // Working with switch statements
+            Console.WriteLine("Working with Switch Statements:");
+            Class41_42.Lessons2.StateSwitch();
+            Console.WriteLine();
+
+            // Displaying a section header for challenges
+            Console.WriteLine("==================================================================================");
+            Console.WriteLine("CHALLENGES");
             //Challengs.Ch1.IsNumberValid();
-            Challengs.Ch2.FindTheMax();
+            //Challengs.Ch2.FindTheMax();
+            Challengs.Ch3.speedLimit();
+            Console.WriteLine("==================================================================================");
         }
     }
-
 }
+
